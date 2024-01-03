8c12f43faa5191f45430b4fcb8e61039457033ec
junarik <113181044+junarik@users.noreply.github.com>
2024-01-02 21:05:12 -0800

    42 done
    to have the checkpoint

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,3 +1,4 @@
+using API.Dtos;
 using Core.Entities;
 using Core.Intrefaces;
 using Core.Specifications;
@@ -25,23 +26,41 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts()
+        public async Task<ActionResult<List<ProductToReturnDto>>> GetProducts()
         {
             var spec = new ProductsWithTypesAndBrandsSpecification();
 
             var products = await _productRepo.ListAsync(spec);
-            
-            return Ok(products);
+
+            return products.Select(product => new ProductToReturnDto
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Description = product.Description,
+                PictureUrl = product.PictureUrl,
+                Price = product.Price,
+                ProductBrand = product.ProductBrand.Name,
+                ProductType = product.ProductType.Name
+            }).ToList();
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Product>> GetProduct(int id)
+        public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
         {
             var spec = new ProductsWithTypesAndBrandsSpecification(id);
 
             var product = await _productRepo.GetEntityWithSpec(spec);
             
-            return Ok(product);
+            return Ok(new ProductToReturnDto
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Description = product.Description,
+                PictureUrl = product.PictureUrl,
+                Price = product.Price,
+                ProductBrand = product.ProductBrand.Name,
+                ProductType = product.ProductType.Name
+            });
         }
 
         [HttpGet("brands")]
